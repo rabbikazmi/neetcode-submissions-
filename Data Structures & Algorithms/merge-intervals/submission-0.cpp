075673669c2a4318fad9a6722ee3f1304d56028d
Rabbika Azmi <azmirabbika@gmail.com>
2026-05-10 13:27:45 +0530

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size()==0) return {};
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> res;
+        vector<int> curr=intervals[0];
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]<=curr[1]){
+                curr[1]=max(curr[1],intervals[i][1]);
+            }else{
+                res.push_back(curr);
+                curr=intervals[i];
+            }
+        }
+        res.push_back(curr);
+        return res;
+    }
+};
